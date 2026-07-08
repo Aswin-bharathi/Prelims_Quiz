e2bace964f971dd395e04ef41c386ab66b779b3f
Subi-83 <k.subitha.83@gmail.com>
2026-07-08 20:59:40 +0530

    Second Commit

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'intercollege_event_2025')
     DB_HOST = os.environ.get('DB_HOST', 'localhost')
     DB_USER = os.environ.get('DB_USER', 'root')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD', 'aswin2772')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD', '8305')
     DB_NAME = os.environ.get('DB_NAME', 'quiz_db')
     DEFAULT_ADMIN_USER = os.environ.get('ADMIN_USER', 'admin')
     DEFAULT_ADMIN_PASS = os.environ.get('ADMIN_PASS', 'admin@123#')
